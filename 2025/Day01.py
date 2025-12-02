bc2ab4d4a65e4580220f4f6c5ba27234e5772dcb
MateoAlonso <73005870+MateoAlonso@users.noreply.github.com>
2025-12-02 12:57:50 -0300

    202501

--- 2025/Day01.py
+++ 2025/Day01.py
@@ -1,17 +1,25 @@
-import math
-INPUT = ["L10", "R50", "R4500", "L0", "R99", "R100"]
+INPUT = [
+    "R1",
+    "L1"
+]
 X = 100
 curr_pos = 50
 answer = 0
 for rota in INPUT:
-    rotaNum = int(rota[:1])
+    rotaNum = int(rota[1:])
 
     if rota[0].lower() == "l":
-        rotaNum *-1;
+        rotaNum *= -1;
 
-    answer += math.floor(rotaNum / x)
-
-    modulo = rotaNum % x
     y = curr_pos + rotaNum
-    if curr_pos + rotaNum < 0:
-        
+    if y < 0:
+        curr_pos = y % X
+    elif y > X:
+        curr_pos = y % X
+    elif y == 0 or y == X:
+        answer += 1
+        curr_pos = 0
+    else:
+        curr_pos = y
+
+print("The answer is: {answer}")
