cf4a89ce9b0c88ddc450d5e708969121b2244513
MateoAlonso <73005870+MateoAlonso@users.noreply.github.com>
2025-12-02 14:44:12 -0300

    202501

--- 2025/Day01.py
+++ 2025/Day01.py
@@ -5,21 +5,22 @@ INPUT = [
 X = 100
 curr_pos = 50
 answer = 0
+total_input = 0
 for rota in INPUT:
-    rotaNum = int(rota[1:])
-
+    rota_num = int(rota[1:])
+    total_input += 1
     if rota[0].lower() == "l":
-        rotaNum *= -1;
+        rota_num *= -1;
 
-    y = curr_pos + rotaNum
-    if y < 0:
-        curr_pos = y % X
-    elif y > X:
+    y = curr_pos + rota_num
+    if y < 0 or y > X:
         curr_pos = y % X
     elif y == 0 or y == X:
-        answer += 1
         curr_pos = 0
     else:
         curr_pos = y
+    
+    if curr_pos == 0:
+        answer += 1
 
-print(f"The answer is: {answer}")
+print(f"The answer is: {answer} and the total input was {total_input}")
