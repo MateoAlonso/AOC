a220c7ad068e3809d2dd953ad1ad72820b834ae5
MateoAlonso <73005870+MateoAlonso@users.noreply.github.com>
2025-12-03 10:11:03 -0300

    202501

--- 2025/Day01.py
+++ 2025/Day01.py
@@ -1,8 +1,8 @@
 INPUT = [
-    "R1",
-    "L1"
+
 ]
 X = 100
+#Part one
 curr_pos = 50
 answer = 0
 total_input = 0
@@ -15,7 +15,7 @@ for rota in INPUT:
     y = curr_pos + rota_num
     if y < 0 or y > X:
         curr_pos = y % X
-    elif y == 0 or y == X:
+    elif y == X:
         curr_pos = 0
     else:
         curr_pos = y
@@ -23,4 +23,35 @@ for rota in INPUT:
     if curr_pos == 0:
         answer += 1
 
-print(f"The answer is: {answer} and the total input was {total_input}")
+print(f"The answer for part one is: {answer} and the total input was {total_input}")
+
+#Part two
+curr_pos = 50
+answer = 0
+total_input = 0
+for rota in INPUT:
+    rota_num = int(rota[1:])
+    total_input += 1
+
+    z = rota_num % X 
+    answer += (rota_num - (z)) / X
+    rota_num = z
+
+    if rota[0].lower() == "l":
+        rota_num *= -1;
+
+    y = curr_pos + rota_num
+    if y < 0 or y > X:
+        if curr_pos != 0:
+            answer += 1
+        curr_pos = y % X
+        
+    elif  y == X:
+        curr_pos = 0
+    else:
+        curr_pos = y
+   
+    if curr_pos == 0 and z!= 0:
+        answer += 1
+
+print(f"The answer for part two is: {answer} and the total input was {total_input}")
